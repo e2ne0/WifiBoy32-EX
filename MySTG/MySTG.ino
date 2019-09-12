bdb4bfe8ca0aa8b27e8c743d9bbe6eefd36c678d
Allen Chen <46492522+e2ne0@users.noreply.github.com>
2019-09-12 11:24:55 +0800

    setup

--- MySTG/MySTG.ino
+++ MySTG/MySTG.ino
@@ -1,3 +1,16 @@
 #include <wifiboy32.h>
 #include <Arduino.h>
 #include "wb32_sprite.h"
+
+void setup()
+{
+    wb32_init();
+    wb32_initBuf8();
+    for (int i = 0; i < 256; i++)
+        wb32_setPal8(i, wb32_color565(sprite_pal[i][0], sprite_pal[i][1], sprite_pal[i][2]));
+}
+
+void loop()
+{
+
+}
