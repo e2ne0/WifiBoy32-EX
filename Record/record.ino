c0c5c7f72cdc3a8efd2fe89da9ede4996cb0400a
Allen Chen <46492522+e2ne0@users.noreply.github.com>
2019-10-04 17:24:06 +0800

    sprite test

--- Record/record.ino
+++ Record/record.ino
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <wifiboy32.h>
+#include "wb32_sprite.h"
 
 void setup()
 {
@@ -9,10 +10,16 @@ void setup()
     pinMode(34, INPUT);
     digitalWrite(22, HIGH);
     digitalWrite(23, HIGH);
+    for (int i = 0; i < 256; i++)
+        wb32_setPal8(i, wb32_color565(sprite_pal[i][0], sprite_pal[i][1], sprite_pal[i][2]));
+    
+    wb32_init();
+    wb32_initBuf8();
 }
 
 void loop()
 {
+    wb32_clearBuf8();
     if (digitalRead(35) == 0)
     {
         digitalWrite(23, 0);
@@ -30,4 +37,6 @@ void loop()
     {
         digitalWrite(22, 0);
     }
+    wb32_blitBuf8(0, 0, 240,0,0,240,320, (uint8_t *)spritesTest);
+    wb32_blit8();
 }
